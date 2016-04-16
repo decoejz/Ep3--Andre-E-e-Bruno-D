0c75988f958cbe8bcef759d7130a7c80c1d3c10e
brunodratcu <btdratcu@hotmail.com>
2016-04-16 07:11:05 -0300

    coloquei 'selfs'

--- EP3.py
+++ EP3.py
@@ -11,3 +11,7 @@ class Jogo:
     """Classe que representa o gerenciamento do jogo"""
     
     def __init__(self, recebe_jogada, verifica_ganhador, limpa_jogadas, off):
+        self.recebe_jogada = recebe_jogada
+        self.verifica_ganhador = verifica_ganhador
+        self.limpa_jogadas = limpa_jogadas
+        self.off = off
