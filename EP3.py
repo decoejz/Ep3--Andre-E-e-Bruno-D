ba1a88084cd23a50c13962a004d3f464f3e053cf
brunodratcu <btdratcu@hotmail.com>
2016-04-16 07:10:07 -0300

    criei a classe jogo

---
+++ EP3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 07:07:47 2016
+
+@author: Bruno Dratcu
+"""
+
+#EP3
+
+class Jogo:
+    """Classe que representa o gerenciamento do jogo"""
+    
+    def __init__(self, recebe_jogada, verifica_ganhador, limpa_jogadas, off):
