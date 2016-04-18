07f73cc392c343d12e4cbcba6e7f70164c241c49
brunodratcu <btdratcu@hotmail.com>
2016-04-18 13:05:18 -0300

    coloquei ifs

--- EP3.py
+++ EP3.py
@@ -7,11 +7,28 @@ Created on Sat Apr 16 07:07:47 2016
 
 #EP3
 
+import numpy as np
+
 class Jogo:
     """Classe que representa o gerenciamento do jogo"""
     
-    def __init__(self, recebe_jogada, verifica_ganhador, limpa_jogadas, off):
-        self.recebe_jogada = recebe_jogada
-        self.verifica_ganhador = verifica_ganhador
-        self.limpa_jogadas = limpa_jogadas
-        self.off = off
+    def __init__(self, X, O, vazio, tabuleiro):
+        self.X = X
+        self.O = O
+        self.vazio = vazio
+        self.tabuleiro = tabuleiro
+        
+    def tabuleiro(self):
+        self.tabuleiro = np.zeros((3,3))
+         
+    def verifica_ganhador(self):
+        if self.tabuleiro == self.vazio:
+            print("Empate!")
+        elif self.X == [0,1,2] or self.X == [3,4,5] or self.X == [6,7,8] or self.X == [0,3,6] or self.X == [1,4,7] or self.X == [2,5,8] or self.X == [0,4,8] or self.X == [2,4,6]:
+            print("O jogador X venceu!")
+        elif self.O == [0,1,2] or self.O == [3,4,5] or self.O == [6,7,8] or self.O == [0,3,6] or self.O == [1,4,7] or self.O == [2,5,8] or self.O == [0,4,8] or self.O == [2,4,6]:
+            print("O jogador O venceu!")
+        
+    def troca_usuario(self):
+        self.X = True
+        
