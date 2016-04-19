ad2ee54963fe15452ac2a145caf48793824bd092
André Ejzenmesser <Andre@MacBook-Air-de-Andre.local>
2016-04-19 09:51:06 -0300

    melhorias

--- Jogo da velha - tkabuleiro.py
+++ Jogo da velha - tkabuleiro.py
@@ -80,21 +80,27 @@ class jogo_da_velha:
         self.window.mainloop()
         self.proxima_jogada(1)
         
-    def proxima_jogada(self,jogador):
-        self.jogod.verifica_ganhador()
+#    def proxima_jogada(self,jogador):
+#        self.jogod.verifica_ganhador()
 
     def clicar_botao_1(self, event):
         self.botao1 = tk.Label(self.window)
-        self.botao1.configure(text='X')
-        self.botao1.configure(state='disabled')
-        self.jogod.recebe_jogada(0,0)
-        self.proxima_jogada()
+        self.jogada_feita = self.jogod.recebe_jogada(0,0)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
         print ('oi')
         
     def clicar_botao_2(self, event):
         self.botao2 = tk.Label(self.window)
         self.botao2.config(text='X')
-        self.jogod.recebe_jogada(0,1)
+        self.jogada_feita = self.jogod.recebe_jogada(0,1)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
@@ -102,49 +108,83 @@ class jogo_da_velha:
     def clicar_botao_3(self, event):
         self.botao3 = tk.Label(self.window)
         self.botao3.configure(text='X')
-        self.jogod.recebe_jogada(0,2)
+        self.jogada_feita = self.jogod.recebe_jogada(0,2)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_4(self, event):
         self.botao4 = tk.Label(self.window)
         self.botao4.configure(text='X')
-        self.jogod.recebe_jogada(1,0)
+        self.jogada_feita = self.jogod.recebe_jogada(1,0)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_5(self, event):
         self.botao5 = tk.Label(self.window)
         self.botao5.configure(text='X')
-        self.jogod.recebe_jogada(1,1)
+        self.jogada_feita = self.jogod.recebe_jogada(1,1)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_6(self, event):
         self.botao6 = tk.Label(self.window)
         self.botao6.configure(text='X')
-        self.jogod.recebe_jogada(1,2)
+        self.jogada_feita = self.jogod.recebe_jogada(1,2)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_7(self, event):
         self.botao7 = tk.Label(self.window)
         self.botao7.configure(text='X')
-        self.jogod.recebe_jogada(2,0)
+        self.jogada_feita = self.jogod.recebe_jogada(2,0)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_8(self, event):
         self.botao8 = tk.Label(self.window)
         self.botao8.configure(text='X')
-        self.jogod.recebe_jogada(2,1)
+        self.jogada_feita = self.jogod.recebe_jogada(2,1)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
+
         #self.proxima_jogada()
         print ('oi')
         
     def clicar_botao_9(self, event):
         self.botao9 = tk.Label(self.window)
         self.botao9.configure(text='X')
-        self.jogod.recebe_jogada(2,2)
+        self.jogada_feita = self.jogod.recebe_jogada(2,2)
+        if self.jogada_feita == 1:
+            self.botao1.configure(text = 'X')
+        elif self.jogada_feita == 2:
+            self.botao1.configure(text='O')
         self.proxima_jogada()
         print ('oi')
     
