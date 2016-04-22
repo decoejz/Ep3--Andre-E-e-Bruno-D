a3ed22de8b527a515a1c5b22f31097472624963f
André Ejzenmesser <Andre@MacBook-Air-de-Andre.local>
2016-04-22 16:41:42 -0300

    Ultimos testes!

--- EP3.py
+++ EP3.py
@@ -33,49 +33,49 @@ class Jogo:
     
     def verifica_ganhador(self):
         #Todas as possibilidades de vencer o jogo
-        if self.tabuleiro[0][0] == self.tabuleiro[0][1] and self.tabuleiro[0][1] == self.tabuleiro[0][2]:
+        if self.tabuleiro[0][0] == self.tabuleiro[0][1] == self.tabuleiro[0][2] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[0][0] == 1:
                 return 1
             elif self.tabuleiro[0][0] == 2:
                 return 2
     
-        elif self.tabuleiro[1][0] == self.tabuleiro[1][1] and self.tabuleiro[1][1] == self.tabuleiro[1][2]:
+        elif self.tabuleiro[1][0] == self.tabuleiro[1][1] == self.tabuleiro[1][2] and self.tabuleiro[1][0]!=0:
             if self.tabuleiro[1][0] == 1:
                 return 1
             elif self.tabuleiro[1][0] == 2:
                 return 2
                 
-        elif self.tabuleiro[2][0] == self.tabuleiro[2][1] and self.tabuleiro[2][1] == self.tabuleiro[2][2]:
+        elif self.tabuleiro[2][0] == self.tabuleiro[2][1] == self.tabuleiro[2][2] and self.tabuleiro[2][0]!=0:
             if self.tabuleiro[2][0] == 1:
                 return 1
             elif self.tabuleiro[2][0] == 2:
                 return 2
                 
-        elif self.tabuleiro[0][0] == self.tabuleiro[1][0] and self.tabuleiro[1][0] == self.tabuleiro[2][0]:
+        elif self.tabuleiro[0][0] == self.tabuleiro[1][0] == self.tabuleiro[2][0] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[0][0] == 1:
                 return 1
             elif self.tabuleiro[0][0] == 2:
                 return 2
                 
-        elif self.tabuleiro[0][1] == self.tabuleiro[1][1] and self.tabuleiro[1][1] == self.tabuleiro[2][1]:
+        elif self.tabuleiro[0][1] == self.tabuleiro[1][1] == self.tabuleiro[2][1] and self.tabuleiro[0][1]!=0:
             if self.tabuleiro[0][1] == 1:
                 return 1
             elif self.tabuleiro[0][1] == 2:
                 return 2
                 
-        elif self.tabuleiro[0][2] == self.tabuleiro[1][2] and self.tabuleiro[1][2] == self.tabuleiro[2][2]:
+        elif self.tabuleiro[0][2] == self.tabuleiro[1][2] == self.tabuleiro[2][2] and self.tabuleiro[0][2]!=0:
             if self.tabuleiro[1][2] == 1:
                 return 1
             elif self.tabuleiro[1][2] == 2:
                 return 2
                 
-        elif self.tabuleiro[0][0] == self.tabuleiro[1][1] and self.tabuleiro[1][1] == self.tabuleiro[2][2]:
+        elif self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[1][1] == 1:
                 return 1
             elif self.tabuleiro[1][1] == 2:
                 return 2
         
-        elif self.tabuleiro[0][2] == self.tabuleiro[1][1] and self.tabuleiro[1][1] == self.tabuleiro[2][0]:
+        elif self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] and self.tabuleiro[0][2]!=0:
             if self.tabuleiro[1][1] == 1:
                 return 1
             elif self.tabuleiro[1][1] == 2:
@@ -83,7 +83,7 @@ class Jogo:
                 
         for linha in range(self.tabuleiro.shape[0]):
             for coluna in range(self.tabuleiro.shape[1]):
-                if self.tabuleiro[linha,coluna] == 0:
+                if self.tabuleiro[linha][coluna] == 0:
                     return -1
         return 0
     
