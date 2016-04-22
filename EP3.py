00b81161cddb63d85b81cecafcc49ad7259990db
André Ejzenmesser <Andre@MacBook-Air-de-Andre.local>
2016-04-22 16:44:36 -0300

    TRABALHO FINALIZADO!!!

--- EP3.py
+++ EP3.py
@@ -36,49 +36,49 @@ class Jogo:
         if self.tabuleiro[0][0] == self.tabuleiro[0][1] == self.tabuleiro[0][2] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[0][0] == 1:
                 return 1
-            elif self.tabuleiro[0][0] == 2:
+            else:
                 return 2
     
         elif self.tabuleiro[1][0] == self.tabuleiro[1][1] == self.tabuleiro[1][2] and self.tabuleiro[1][0]!=0:
             if self.tabuleiro[1][0] == 1:
                 return 1
-            elif self.tabuleiro[1][0] == 2:
+            else:
                 return 2
                 
         elif self.tabuleiro[2][0] == self.tabuleiro[2][1] == self.tabuleiro[2][2] and self.tabuleiro[2][0]!=0:
             if self.tabuleiro[2][0] == 1:
                 return 1
-            elif self.tabuleiro[2][0] == 2:
+            else:
                 return 2
                 
         elif self.tabuleiro[0][0] == self.tabuleiro[1][0] == self.tabuleiro[2][0] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[0][0] == 1:
                 return 1
-            elif self.tabuleiro[0][0] == 2:
+            else:
                 return 2
                 
         elif self.tabuleiro[0][1] == self.tabuleiro[1][1] == self.tabuleiro[2][1] and self.tabuleiro[0][1]!=0:
             if self.tabuleiro[0][1] == 1:
                 return 1
-            elif self.tabuleiro[0][1] == 2:
+            else:
                 return 2
                 
         elif self.tabuleiro[0][2] == self.tabuleiro[1][2] == self.tabuleiro[2][2] and self.tabuleiro[0][2]!=0:
             if self.tabuleiro[1][2] == 1:
                 return 1
-            elif self.tabuleiro[1][2] == 2:
+            else:
                 return 2
                 
         elif self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] and self.tabuleiro[0][0]!=0:
             if self.tabuleiro[1][1] == 1:
                 return 1
-            elif self.tabuleiro[1][1] == 2:
+            else:
                 return 2
         
         elif self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] and self.tabuleiro[0][2]!=0:
             if self.tabuleiro[1][1] == 1:
                 return 1
-            elif self.tabuleiro[1][1] == 2:
+            else:
                 return 2
                 
         for linha in range(self.tabuleiro.shape[0]):
